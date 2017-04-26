60a4a9fea5c4d80eb2c57368c884e5725f7e27e0
Paweł Jarosz <paweljarosz3691@gmail.com>
2017-04-26 10:13:39 +0200

    rockchip: serial: support rockchip rk3066
    update driver to support rk3066 serial
    
    Signed-off-by: Paweł Jarosz <paweljarosz3691@gmail.com>

--- drivers/serial/serial_rockchip.c
+++ drivers/serial/serial_rockchip.c
@@ -12,7 +12,14 @@
 #include <serial.h>
 #include <asm/arch/clock.h>
 
-#if defined(CONFIG_ROCKCHIP_RK3188)
+
+#if defined(CONFIG_ROCKCHIP_RK3066)
+struct rockchip_uart_platdata {
+	struct dtd_rockchip_rk3066_uart dtplat;
+	struct ns16550_platdata plat;
+};
+struct dtd_rockchip_rk3066_uart *dtplat, s_dtplat;
+#elif defined(CONFIG_ROCKCHIP_RK3188)
 struct rockchip_uart_platdata {
 	struct dtd_rockchip_rk3188_uart dtplat;
 	struct ns16550_platdata plat;
@@ -39,6 +46,16 @@ static int rockchip_serial_probe(struct udevice *dev)
 
 	return ns16550_serial_probe(dev);
 }
+
+U_BOOT_DRIVER(rockchip_rk3066_uart) = {
+	.name	= "rockchip_rk3066_uart",
+	.id	= UCLASS_SERIAL,
+	.priv_auto_alloc_size = sizeof(struct NS16550),
+	.platdata_auto_alloc_size = sizeof(struct rockchip_uart_platdata),
+	.probe	= rockchip_serial_probe,
+	.ops	= &ns16550_serial_ops,
+	.flags	= DM_FLAG_PRE_RELOC,
+};
 
 U_BOOT_DRIVER(rockchip_rk3188_uart) = {
 	.name	= "rockchip_rk3188_uart",
