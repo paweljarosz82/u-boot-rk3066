587dcf675b13af392dd250fbf967c4385e4331a6
Paweł Jarosz <paweljarosz3691@gmail.com>
2022-03-06 20:49:17 +0100

    rockusb: add base support
    Add common base support for mmc and nand.
    We intend to support different devices in rockusb like mmc and nand
    and this is skeleton for this support.
    
    Signed-off-by: Paweł Jarosz <paweljarosz3691@gmail.com>

---
+++ drivers/rockusb/rockusb.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2022 Pawel Jarosz <paweljarosz3691@gmail.com>
+ */
+
+#include <common.h>
+#include <linux/usb/composite.h>
+#include <asm/arch-rockchip/f_rockusb.h>
+
+struct rockusb_dev_desc *rockusb_get_dev(char *dev_type,
+					 unsigned int dev_index)
+{
+}
